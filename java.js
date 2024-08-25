aee5ba350572fcc8cccc0fa0b1d42bd10870d5cd
Useftarek <useftarek2@gmail.com>
2024-08-25 03:27:17 +0200

    done

--- java.js
+++ java.js
@@ -5,18 +5,16 @@ const cards = [
     {title:"Perfected Strategies for you", desc: "Lorem ipsum dolor sit amet consectetur. Id est convallis neque elementum.", img1: "./a22c95a96b25b207b760ab6545995150.png", img2:"./b3de947c39102b151251150791b5ba1e.png"},
     {title:"Perfected Strategies for you", desc: "Lorem ipsum dolor sit amet consectetur. Id est convallis neque elementum.", img1: "./a22c95a96b25b207b760ab6545995150.png", img2:"./b3de947c39102b151251150791b5ba1e.png"},
 ];
-for (let i = 0; i< cards.length; i++) {
-    
-    container.innerHTML += ` <div class="cardContainer">
-    <div class="card">
-        <img src="${cards [i].img1}">
-    </div>
-    <div class="adid">
-        <img src="${cards [i].img2}">
-    </div>
-    <div class="description">
-        <h2>${cards [i].title}</h2>
-        <p>${cards [i].desc}</p>
-    </div>
-</div>`
-};
+
+cards.forEach((data)=>container.innerHTML += ` <div class="cardContainer">
+      <div class="card">
+          <img src="${data.img1}">
+      </div>
+      <div class="adid">
+          <img src="${data.img2}">
+     </div>
+   <div class="description">
+          <h2>${data.title}</h2>
+          <p>${data.desc}</p>
+     </div>
+  </div>`);
